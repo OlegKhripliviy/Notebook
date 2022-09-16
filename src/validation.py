e5106ad1057aea23305c71fe8a53d4ed575ddda2
oleg <olegkhripliviy@gmail.com>
2022-09-16 19:23:03 +0300

    update files

--- src/validation.py
+++ src/validation.py
@@ -6,44 +6,67 @@ us = UserInterface()
 
 class Validation:
     def __init__(self):
-        pass
+        self.note_list = []
 
     @staticmethod
-    def name_valid(name):
-        if len(name) < 2 or not name.isalpha():
-            print("First or last name is too short or has number. Try again\n")
-            return False
-        else:
-            return True
+    def first_name_valid():
+        while True:
+            name = us.add_first_name()
+            if len(name) < 2 or not name.isalpha():
+                print("Too short or has number. Try again\n")
+            else:
+                return name
 
     @staticmethod
-    def number_valid(number):
-        if len(number) != 10 or not number.isdigit():
-            print("Incorrect input phone number. Try again\n")
-            return False
-        else:
-            return True
+    def last_name_valid():
+        while True:
+            name = us.add_last_name()
+            if len(name) < 2 or not name.isalpha():
+                print("Too short or has number. Try again\n")
+            else:
+                return name
 
     @staticmethod
-    def date_valid(date):
-        try:
-            if datetime.strptime(date, '%m.%d.%Y'):
-                return date
-        except ValueError:
-            print("Invalid data. Try again")
-            return False
+    def number_valid():
+        while True:
+            number = us.add_number()
+            if len(number) != 10 or not number.isdigit():
+                print("Incorrect input phone number. Try again\n")
+            else:
+                return number
+
+    @staticmethod
+    def date_valid():
+        while True:
+            date = us.add_date()
+            try:
+                if datetime.strptime(date, '%d.%m.%Y'):
+                    return date
+            except ValueError:
+                print("Invalid data. Try again")
+                return False
 
     def add_note_validation(self):
-        records = us.add_note_interface()
-        if not self.name_valid(records["first_name"]):
-            self.add_note_validation()
-        elif not self.name_valid(records["last_name"]):
-            self.add_note_validation()
-        elif not self.number_valid(records["number"]):
-            self.add_note_validation()
-        if not self.date_valid(records["date"]):
-            self.add_note_validation()
-        return records
+        self.note_list.append(self.first_name_valid())
+        self.note_list.append(self.last_name_valid())
+        self.note_list.append(self.number_valid())
+        self.note_list.append(us.add_address())
+        self.note_list.append(self.date_valid())
+        return self.note_list
 
 
-print(Validation().add_note_validation())
+    def del_note_validation(self):
+        while True:
+            del_or_ex = us.input_del_or_ex()
+            if del_or_ex != '0' and del_or_ex != '1':
+                print("Unknown command. Try again")
+            elif del_or_ex == '0':
+                break
+            try:
+                note_id = int(us.input_id())
+                return note_id
+            except ValueError:
+                print("You didn't enter a number. Try again")
+
+
+print(Validation().del_note_validation())
